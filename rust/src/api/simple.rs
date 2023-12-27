a6f7a1ae1aea4e9d7b42c51053ab82b619007278
Amman Zaman <amman.z.zaman@gmail.com>
2023-12-27 12:51:16 -0800

    removed prints in rust, separate issue

--- rust/src/api/simple.rs
+++ rust/src/api/simple.rs
@@ -3,29 +3,24 @@ use std::panic::UnwindSafe;
 use std::{thread, time};
 
 async fn random_async() -> anyhow::Result<()> {
-    println!("RUST: this won't get printed on any platform, sleeping");
     thread::sleep(time::Duration::from_millis(400));
-    println!("RUST: this won't get printed on any platform, finished sleeping");
     Ok(())
 }
 
 // this function will return but println won't output anything to stdout
 #[flutter_rust_bridge::frb(sync)] // Synchronous mode for simplicity of the demo
 pub fn sync_greet(name: String) -> String {
-    println!("RUST: this won't get printed on any platform, sync_greet received name: {name}");
     format!("sync_greet: Hello, {name}!")
 }
 
 // this function will never return on Android SDK <=30 and both println and dart logger won't output anything to stdout
 pub async fn async_greet_with_callback(name: String, logger: impl Fn(String) -> DartFnFuture<()> + UnwindSafe) -> anyhow::Result<String> {
-    println!("RUST: this won't get printed on any platform, async_greet_with_callback received name: {name}");
     logger(format!("DART FROM RUST: this won't get printed on Android SDK <=30, async_greet_with_callback received name: {name}").to_owned()).await;
     Ok(format!("async_greet_with_callback: Hello, {name}!"))
 }
 
 // this function will return but both println and dart logger won't print anything
 pub async fn async_greet_with_callback_no_await(name: String, logger: impl Fn(String) -> DartFnFuture<()> + UnwindSafe) -> anyhow::Result<String> {
-    println!("RUST: this won't get printed, async_greet_with_callback_no_await received name: {name}");
     logger(format!("DART FROM RUST: this won't get printed on any platform, async_greet_with_callback_no_await received name: {name}").to_owned());
     Ok(format!("async_greet_with_callback_no_await: Hello, {name}!"))
 }
@@ -33,7 +28,6 @@ pub async fn async_greet_with_callback_no_await(name: String, logger: impl Fn(St
 
 // this function will never return on Android SDK <=30 and both println and dart logger won't output anything to stdout
 pub async fn async_greet(name: String) -> anyhow::Result<String> {
-    println!("RUST: this won't get printed on any platform, async_greet received name: {name}");
     let _ = random_async().await;
     Ok(format!("async_greet: Hello, {name}!"))
 }
