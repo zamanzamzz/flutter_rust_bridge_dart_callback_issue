a2fbb46993ebda6da1ef02dafc4e2472bedc9bf4
Amman Zaman <amman.z.zaman@gmail.com>
2023-12-26 21:22:55 -0800

    fixed formating of DART FROM RUST logs

--- rust/src/api/simple.rs
+++ rust/src/api/simple.rs
@@ -11,13 +11,13 @@ pub fn greet(name: String) -> String {
 // this function will never return and both println and dart logger won't output anything to stdout
 pub async fn async_greet(name: String, logger: impl Fn(String) -> DartFnFuture<()> + UnwindSafe) -> anyhow::Result<String> {
     println!("RUST: this won't get printed, async greet received name: {name}");
-    logger("DART FROM RUST: this won't get printed, async greet received name: {name}".to_owned()).await;
+    logger(format!("DART FROM RUST: this won't get printed, async greet received name: {name}").to_owned()).await;
     Ok(format!("async_greet: Hello, {name}!"))
 }
 
 // this function will return but both println and dart logger won't print anything
 pub async fn async_no_await_greet(name: String, logger: impl Fn(String) -> DartFnFuture<()> + UnwindSafe) -> anyhow::Result<String> {
     println!("RUST: this won't get printed, async greet received name: {name}");
-    logger("DART FROM RUST: this won't get printed, async greet received name: {name}".to_owned());
+    logger(format!("DART FROM RUST: this won't get printed, async greet received name: {name}").to_owned());
     Ok(format!("async_no_await_greet: Hello, {name}!"))
 }
